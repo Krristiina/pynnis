174ee2c78c7b0e06e10bc8faa9b5329f6ac94d3e
Erkki-Ilmari Rajakoski <erkki@studiorajakoski.com>
2018-06-10 12:34:24 +0300

    Ball added to field

--- run_game.py
+++ run_game.py
@@ -23,10 +23,15 @@ FIELD_Y = 0.1 * display_height
 FIELD_WIDTH = 0.8 * display_width
 FIELD_HEIGHT = 0.8 * display_height
 
+# player positions
 PLAYER1_X = FIELD_X + 20
 PLAYER2_X = FIELD_X + FIELD_WIDTH - 20
 PLAYER_MIN_Y = FIELD_Y
 PLAYER_MAX_Y = FIELD_Y + FIELD_HEIGHT
+
+# ball initial position
+BALL_INIT_X = FIELD_X + 30
+BALL_INIT_Y = FIELD_Y + 50
 
 # initializing pygame & creating window
 pygame.init()
@@ -39,9 +44,11 @@ all_sprites = pygame.sprite.Group()
 field = Field(FIELD_X, FIELD_Y, FIELD_WIDTH, FIELD_HEIGHT, green)
 player1 = Player(PLAYER1_X, PLAYER_MIN_Y, PLAYER_MAX_Y, red, pygame.K_a, pygame.K_s)
 player2 = Player(PLAYER2_X, PLAYER_MIN_Y, PLAYER_MAX_Y, red, pygame.K_k, pygame.K_l)
+ball = Ball(BALL_INIT_X, BALL_INIT_Y, black)
 all_sprites.add(field)
 all_sprites.add(player1)
 all_sprites.add(player2)
+all_sprites.add(ball)
 
 quitted = False
 # Creating game loop
--- sprites/ball.py
+++ sprites/ball.py
@@ -1,9 +1,17 @@
 import pygame
 
 class Ball(pygame.sprite.Sprite):
+    
+    arc = 8
 
-    def __init__(self, init_x, init_y, color, down_key, up_key):
+    def __init__(self, init_x, init_y, color):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((self.racket_height, self.racket_width))
-        self.image.fill(color)
+        self.image = pygame.Surface((self.arc * 2, self.arc * 2), pygame.SRCALPHA)
+        self.image.convert_alpha()
+        pygame.draw.circle(self.image, color, (self.arc, self.arc), self.arc, 0)
         self.rect = self.image.get_rect()
+        self.rect.x = init_x
+        self.rect.y = init_y
+
+    def update(self):
+        pass
